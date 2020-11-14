df4db2a59758e63519e5d7e65533422a67628dca
Gabriel-Garcia2004 <garcia.gabrielrs@gmail.com>
2020-11-14 12:20:20 -0300

    dia 6

---
+++ projeto-glass-html5/_javascript/funcoes.js
@@ -0,0 +1,10 @@
+  function mudaFoto(foto) {
+    /*document.querySelector('#icone').src = null*/
+    setTimeout(function() {
+    document.querySelector('#icone').src = foto
+  
+   
+  } , 400)
+ 
+  }
+ 
